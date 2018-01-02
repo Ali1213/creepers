8143cc085f44fc11d3bc57f1030ebbb0ebf0644f
Ali1213 <yusili2000@163.com>
2018-01-03 00:00:59 +0800

    add: convertTraitToUrl

--- USNEWSranks/USNEWSranks.js
+++ USNEWSranks/USNEWSranks.js
@@ -7,27 +7,40 @@ const public = require('../public');
 const got = require('got');
 
 
-
 const getAllRankTagObjs = async function(mainUrl){
   const html = await got(mainUrl);
   const $ = cheerio.load(html.body);
   const $option = $("[name='school-type'] option");
-  let tagsObj = [];
+  let tagsArr = [];
   for(let tag of $option){
     let $tags = cheerio.load(tag);
     if($tags.attr('value').trim()){
-      tagsObj.push({
-        tag:$tags.attr('value'),
+      tagsArr.push({
+        trait:$tags.attr('value'),
         name:$tags.text().trim()
-      })
+      });
     }
   }
-  return tagsObj;
+  return tagsArr;
+}
+
+const convertTraitToUrl = function (tagsArr){
+  return tagsArr.map(item => {
+    return {
+      url: config.USNEWSranks.ranksUrlEx.replace('national-universities',item.trait),
+      name: item.name,
+    }
+  });
 }
 
 
+const requestAndWriteToDB = async function(obj){
+  
+}
+
 const getUniversityRanks = async function(mainUrl = config.USNEWSranks.universityRanksUrl){
-  const tagObj = getAllRankTagObjs(mainUrl);
+  const tagsArr = await getAllRankTagObjs(mainUrl);
+  const tagUrls = convertTraitToUrl(tagsArr);
 }
 
 
